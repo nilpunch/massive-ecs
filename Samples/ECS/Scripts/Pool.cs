631c43a8ef6ad3f49e7bdc6c57d31a6d36c429e3
Daniil Pankevich <daniil.pankevich@gmail.com>
2024-02-19 00:12:35 +0300

    Folders refactoring, new sample WIP

---
+++ Samples/ECS/Scripts/Pool.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using MassiveData;
+using UnityEngine;
+
+namespace Massive.Samples.ECS
+{
+    public class Pool
+    {
+        
+    }
+
+    public class EcsWorld
+    {
+        public MassiveSparseSet Entities;
+    }
+}
