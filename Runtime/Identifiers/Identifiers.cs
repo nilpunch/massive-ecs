4956ac9491af1debd01f3fd60270ec382c994861
Daniil Pankevich <daniil.pankevich@gmail.com>
2024-03-06 13:24:02 +0300

    Add Identifiers data structure

---
+++ Runtime/Identifiers/Identifiers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Massive.Plugins.massive.Runtime.Identifiers
+{
+	public class Identifiers
+	{
+		public int[] Ids { get; }
+		public int MaxId { get; set; }
+
+		// Recycling
+		public int Available { get; set; }
+		public int Next { get; set; }
+
+		public Identifiers(int dataCapacity = Constants.DataCapacity)
+		{
+			Ids = new int[dataCapacity];
+			Next = dataCapacity;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public int Create()
+		{
+			int maxId = MaxId;
+
+			if (maxId >= Ids.Length)
+			{
+				throw new InvalidOperationException($"Exceeded limit of ids! Limit: {Ids.Length}.");
+			}
+
+			if (Available == 0)
+			{
+				MaxId += 1;
+				Ids[maxId] = maxId;
+				return maxId;
+			}
+			else
+			{
+				var recycledId = Next;
+				Next = Ids[Next];
+				Available -= 1;
+				return recycledId;
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Delete(int id)
+		{
+			if (!IsAlive(id))
+			{
+				return;
+			}
+
+			(Next, Ids[id]) = (id, Next);
+			Available += 1;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool IsAlive(int id)
+		{
+			return id < MaxId && Ids[id] == id;
+		}
+	}
+}
---
+++ Runtime/Identifiers/MassiveIdentifiers.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Unity.IL2CPP.CompilerServices;
+
+namespace Massive.Plugins.massive.Runtime.Identifiers
+{
+	[Il2CppSetOption(Option.NullChecks, false)]
+	[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+	[Il2CppSetOption(Option.DivideByZeroChecks, false)]
+	public class MassiveIdentifiers : Identifiers, IMassive
+	{
+		private readonly int[] _idsByFrames;
+		private readonly int[] _maxIdByFrames;
+		private readonly int[] _availableByFrames;
+		private readonly int[] _nextByFrames;
+
+		private readonly int _framesCapacity;
+		private int _currentFrame;
+		private int _savedFrames;
+
+		public MassiveIdentifiers(int framesCapacity = Constants.FramesCapacity, int dataCapacity = Constants.DataCapacity)
+			: base(dataCapacity)
+		{
+			_framesCapacity = framesCapacity;
+
+			_idsByFrames = new int[framesCapacity * Ids.Length];
+			_maxIdByFrames = new int[framesCapacity];
+			_availableByFrames = new int[framesCapacity];
+			_nextByFrames = new int[framesCapacity];
+		}
+
+		public int CurrentFrame => _currentFrame;
+
+		/// <summary>
+		/// Can be negative, when there absolutely no saved frames to restore information.
+		/// </summary>
+		public int CanRollbackFrames => _savedFrames - 1;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void SaveFrame()
+		{
+			int currentMaxId = MaxId;
+			int currentAvailable = Available;
+			int currentNext = Next;
+			int nextFrame = Loop(_currentFrame + 1, _framesCapacity);
+
+			// Copy everything from current state to next frame
+			Array.Copy(Ids, 0, _idsByFrames, nextFrame * Ids.Length, currentMaxId);
+			_maxIdByFrames[nextFrame] = currentMaxId;
+			_availableByFrames[nextFrame] = currentAvailable;
+			_nextByFrames[nextFrame] = currentNext;
+
+			_currentFrame = nextFrame;
+			_savedFrames = Math.Min(_savedFrames + 1, _framesCapacity);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Rollback(int frames)
+		{
+			if (frames > CanRollbackFrames)
+			{
+				throw new InvalidOperationException($"Can't rollback this far. CanRollback:{CanRollbackFrames}, Requested: {frames}.");
+			}
+
+			_savedFrames -= frames;
+			_currentFrame = LoopNegative(_currentFrame - frames, _framesCapacity);
+
+			// Copy everything from rollback frame to current state
+			int rollbackMaxId = _maxIdByFrames[_currentFrame];
+			int rollbackAvailable = _availableByFrames[_currentFrame];
+			int rollbackNext = _nextByFrames[_currentFrame];
+			int rollbackFrame = _currentFrame;
+
+			// Copy current MaxId elements to ensure zeroing excess
+			Array.Copy(_idsByFrames, rollbackFrame * Ids.Length, Ids, 0, MaxId);
+			MaxId = rollbackMaxId;
+			Available = rollbackAvailable;
+			Next = rollbackNext;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static int Loop(int a, int b)
+		{
+			return a % b;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static int LoopNegative(int a, int b)
+		{
+			int result = a % b;
+
+			if (result < 0)
+			{
+				return result + b;
+			}
+
+			return result;
+		}
+	}
+}
