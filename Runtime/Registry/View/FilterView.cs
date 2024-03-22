1441d9651f117c11cf3a3554b9e7311ed0037903
Daniil Pankevich <daniil.pankevich@gmail.com>
2024-03-22 16:48:47 +0300

    Add more views

---
+++ Runtime/Registry/View/FilterView.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Unity.IL2CPP.CompilerServices;
+
+namespace Massive
+{
+	[Il2CppSetOption(Option.NullChecks, false)]
+	[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+	public readonly struct FilterView
+	{
+		private readonly Identifiers _entities;
+		private readonly ISet[] _include;
+		private readonly ISet[] _exclude;
+
+		public FilterView(IRegistry registry, ISet[] include = null, ISet[] exclude = null)
+		{
+			_entities = registry.Entities;
+			_include = include ?? Array.Empty<ISet>();
+			_exclude = exclude ?? Array.Empty<ISet>();
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void ForEach(EntityAction action)
+		{
+			var ids = _include.Length == 0
+				? _entities.AliveIds
+				: ViewUtils.GetMinimalSet(_include).AliveIds;
+
+			for (var i = ids.Length - 1; i >= 0; i--)
+			{
+				var id = ids[i];
+				if (ViewUtils.AliveInAll(id, _include) && ViewUtils.NotAliveInAll(id, _exclude))
+				{
+					action.Invoke(id);
+				}
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void ForEachExtra<TExtra>(TExtra extra, EntityActionExtra<TExtra> action)
+		{
+			var ids = _include.Length == 0
+				? _entities.AliveIds
+				: ViewUtils.GetMinimalSet(_include).AliveIds;
+
+			for (var i = ids.Length - 1; i >= 0; i--)
+			{
+				var id = ids[i];
+				if (ViewUtils.AliveInAll(id, _include) && ViewUtils.NotAliveInAll(id, _exclude))
+				{
+					action.Invoke(id, extra);
+				}
+			}
+		}
+	}
+}
---
+++ Runtime/Registry/View/FilterViewTT.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Unity.IL2CPP.CompilerServices;
+
+namespace Massive
+{
+	[Il2CppSetOption(Option.NullChecks, false)]
+	[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+	public readonly struct FilterView<T1, T2>
+		where T1 : struct
+		where T2 : struct
+	{
+		private readonly IDataSet<T1> _components1;
+		private readonly IDataSet<T2> _components2;
+		private readonly ISet[] _exclude;
+		private readonly ISet[] _componentsAndInclude;
+
+		public FilterView(IRegistry registry, ISet[] include = null, ISet[] exclude = null)
+		{
+			_components1 = registry.Components<T1>();
+			_components2 = registry.Components<T2>();
+			_exclude = exclude ?? Array.Empty<ISet>();
+			_componentsAndInclude = new ISet[include.Length + 2];
+			_componentsAndInclude[0] = _components1;
+			_componentsAndInclude[1] = _components2;
+			include.CopyTo(_componentsAndInclude, 2);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void ForEach(EntityActionRef<T1, T2> action)
+		{
+			var data1 = _components1.AliveData;
+			var data2 = _components2.AliveData;
+			var ids = ViewUtils.GetMinimalSet(_componentsAndInclude).AliveIds;
+			Span<int> allDense = stackalloc int[_componentsAndInclude.Length];
+
+			for (int i = ids.Length - 1; i >= 0; i--)
+			{
+				int id = ids[i];
+				if (ViewUtils.TryGetAllDense(id, _componentsAndInclude, allDense) && ViewUtils.NotAliveInAll(id, _exclude))
+				{
+					action.Invoke(id, ref data1[allDense[0]], ref data2[allDense[1]]);
+				}
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void ForEachExtra<TExtra>(TExtra extra, EntityActionRefExtra<T1, T2, TExtra> action)
+		{
+			var data1 = _components1.AliveData;
+			var data2 = _components2.AliveData;
+			var ids = ViewUtils.GetMinimalSet(_componentsAndInclude).AliveIds;
+			Span<int> allDense = stackalloc int[_componentsAndInclude.Length];
+
+			for (int i = ids.Length - 1; i >= 0; i--)
+			{
+				int id = ids[i];
+				if (ViewUtils.TryGetAllDense(id, _componentsAndInclude, allDense) && ViewUtils.NotAliveInAll(id, _exclude))
+				{
+					action.Invoke(id, ref data1[allDense[0]], ref data2[allDense[1]], extra);
+				}
+			}
+		}
+	}
+}
