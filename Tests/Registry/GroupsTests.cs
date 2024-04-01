5a6e914943571963f9fdee9a3e7d01708bc7dbc2
Daniil Pankevich <daniil.pankevich@gmail.com>
2024-04-01 15:24:49 +0300

    Add functionality and exclusion test to all groups

--- Tests/Registry/GroupsTests.cs
+++ Tests/Registry/GroupsTests.cs
@@ -1,34 +1,47 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace Massive.Tests
 {
-	[TestFixture]
+	[TestFixture(TestGroupType.FullOwningGroup)]
+	[TestFixture(TestGroupType.PartialOwningGroup)]
+	[TestFixture(TestGroupType.NonOwningGroup)]
 	public class GroupsTests
 	{
+		public enum TestGroupType
+		{
+			FullOwningGroup,
+			PartialOwningGroup,
+			NonOwningGroup,
+		}
+
+		private readonly TestGroupType _testGroupType;
+
+		public GroupsTests(TestGroupType testGroupType)
+		{
+			_testGroupType = testGroupType;
+		}
+
 		[Test]
-		public void NonOwningGroup_ShouldWork()
+		public void Group_FunctionalityTest()
 		{
 			var registry = new Registry();
 
-			var group = registry.Groups.EnsureGroup(new ISet[]
-			{
-				registry.Components<int>(),
-				registry.Components<char>()
-			});
-			
+			var group = SetUpGroupForFunctionalityTest(registry);
+
 			Assert.AreEqual(group.Length, 0);
 
 			var entity1 = registry.Create('1');
 			var entity2 = registry.Create('2');
 
 			registry.Add(entity2, 42);
-			
+
 			Assert.AreEqual(group.Length, 1);
-			
+
 			registry.Add<int>(entity1);
 
 			Assert.AreEqual(group.Length, 2);
-			
+
 			registry.Remove<int>(entity1);
 
 			Assert.AreEqual(group.Length, 1);
@@ -38,55 +51,129 @@ namespace Massive.Tests
 				Assert.AreEqual(registry.Get<int>(id), 42);
 				Assert.AreEqual(registry.Get<char>(id), '2');
 			}
-			
+
 			Assert.AreEqual(group.Ids[0], entity2);
-			
+
 			registry.Remove<char>(entity1);
 			registry.Remove<char>(entity2);
-			
+
 			Assert.AreEqual(group.Length, 0);
 		}
 
 		[Test]
-		public void OwningGroup_ShouldWork()
+		public void Group_ExclusionTest()
 		{
 			var registry = new Registry();
 
-			var group = registry.Groups.EnsureGroup(include: new IReadOnlySet[]
+			var entity1 = registry.Create(1);
+
+			var entity2 = registry.Create(2);
+			registry.Add<char>(entity2);
+
+			var group = SetUpGroupForExclusionTest(registry);
+
+			var entity3 = registry.Create(3);
+
+			var entity4 = registry.Create(4);
+			registry.Add<char>(entity4);
+
+			foreach (var entity in group.Ids)
 			{
-				registry.Components<int>(),
-				registry.Components<char>()
-			});
-			
-			Assert.AreEqual(group.Length, 0);
+				Assert.True(entity == entity1 || entity == entity3);
 
-			var entity1 = registry.Create('1');
-			var entity2 = registry.Create('2');
-
-			registry.Add(entity2, 42);
-			
-			Assert.AreEqual(group.Length, 1);
-			
-			registry.Add<int>(entity1);
-
-			Assert.AreEqual(group.Length, 2);
-			
-			registry.Remove<int>(entity1);
-
-			Assert.AreEqual(group.Length, 1);
-
-			foreach (var id in group.Ids)
-			{
-				Assert.AreEqual(registry.Get<int>(id), 42);
-				Assert.AreEqual(registry.Get<char>(id), '2');
+				if (entity == entity1)
+				{
+					Assert.AreEqual(registry.Get<int>(entity1), 1);
+				}
+				else if (entity == entity3)
+				{
+					Assert.AreEqual(registry.Get<int>(entity3), 3);
+				}
 			}
-			
-			Assert.AreEqual(group.Ids[0], entity2);
-			
-			registry.Remove<char>(entity1);
-			registry.Remove<char>(entity2);
-			
+
+			registry.Add<char>(entity1);
+			registry.Add<char>(entity3);
+
 			Assert.AreEqual(group.Length, 0);
+
+			registry.Remove<char>(entity2);
+			registry.Remove<char>(entity4);
+
+			foreach (var entity in group.Ids)
+			{
+				Assert.True(entity == entity2 || entity == entity4);
+
+				if (entity == entity2)
+				{
+					Assert.AreEqual(registry.Get<int>(entity2), 2);
+				}
+				else if (entity == entity4)
+				{
+					Assert.AreEqual(registry.Get<int>(entity4), 4);
+				}
+			}
+		}
+
+		private IGroup SetUpGroupForFunctionalityTest(IRegistry registry)
+		{
+			switch (_testGroupType)
+			{
+				case TestGroupType.FullOwningGroup:
+					return registry.Groups.EnsureGroup(owned: new ISet[]
+					{
+						registry.Components<int>(),
+						registry.Components<char>()
+					});
+				case TestGroupType.PartialOwningGroup:
+					return registry.Groups.EnsureGroup(
+						owned: new ISet[]
+						{
+							registry.Components<int>()
+						},
+						include: new IReadOnlySet[]
+						{
+							registry.Components<char>()
+						}
+					);
+				case TestGroupType.NonOwningGroup:
+					return registry.Groups.EnsureGroup(include: new IReadOnlySet[]
+					{
+						registry.Components<int>(),
+						registry.Components<char>()
+					});
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+
+		private IGroup SetUpGroupForExclusionTest(IRegistry registry)
+		{
+			switch (_testGroupType)
+			{
+				case TestGroupType.FullOwningGroup:
+				case TestGroupType.PartialOwningGroup:
+					return registry.Groups.EnsureGroup(
+						owned: new ISet[]
+						{
+							registry.Components<int>(),
+						},
+						exclude: new IReadOnlySet[]
+						{
+							registry.Components<char>()
+						});
+				case TestGroupType.NonOwningGroup:
+					return registry.Groups.EnsureGroup(
+						include: new IReadOnlySet[]
+						{
+							registry.Components<int>(),
+						},
+						exclude: new IReadOnlySet[]
+						{
+							registry.Components<char>()
+						});
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
 		}
 	}
 }
