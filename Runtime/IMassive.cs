d4f6de60a8cca1b71ced5c59033ad6b3c334415d
Daniil Pankevich <daniil.pankevich@gmail.com>
2024-05-04 12:54:19 +0300

    Add IMassive summaries

--- Runtime/IMassive.cs
+++ Runtime/IMassive.cs
@@ -3,12 +3,19 @@
 	public interface IMassive
 	{
 		/// <summary>
-		/// Can be negative, when there absolutely no saved frames to restore information.
+		/// Can be negative when there are absolutely no saved frames to restore data.
 		/// </summary>
 		int CanRollbackFrames { get; }
 
+		/// <summary>
+		/// Saves current frame data.
+		/// </summary>
 		void SaveFrame();
 
+		/// <summary>
+		/// Restores data from some frames ago.<br/>
+		/// </summary>
+		/// <param name="frames">Must be non-negative and not exceed <see cref="IMassive.CanRollbackFrames"/>.</param>
 		void Rollback(int frames);
 	}
 }
