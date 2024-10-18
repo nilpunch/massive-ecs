894c862faa03dd267c355752b8e1ee70c5c341c1
Daniil Pankevich <daniil.pankevich@gmail.com>
2024-10-19 00:46:36 +0300

    Add compaction test

---
+++ Tests/SparseSetTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Massive.Tests
+{
+	[TestFixture]
+	public class SparseSetTests
+	{
+		public int[] IdsToAssign =
+		{
+			0, 1, 2, 3, 4, 5, 6, 7, 20, 21, 22, 23
+		};
+
+		public int[] IdsToUnassign =
+		{
+			0, 1, 3, 20, 21, 23
+		};
+
+		[Test]
+		public void WhenCompact_AndThereIsHoles_ThenShouldRemoveHoles()
+		{
+			// Arrange
+			var sparseSet = new SparseSet(packingMode: PackingMode.WithHoles);
+			foreach (var id in IdsToAssign)
+				sparseSet.Assign(id);
+			foreach (var id in IdsToUnassign)
+				sparseSet.Unassign(id);
+
+			// Act
+			sparseSet.Compact();
+
+			// Assert
+			int remainIdsCount = IdsToAssign.Length - IdsToUnassign.Length;
+			Assert.AreEqual(remainIdsCount, sparseSet.Count);
+		}
+	}
+}
