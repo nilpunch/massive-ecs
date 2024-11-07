a69c08be8ebb82be52d6558bb390fd3a1fa0ebf1
Daniil Pankevich <daniil.pankevich@gmail.com>
2024-11-07 22:26:11 +0300

    Add Il2CppSetOption to everything

--- Runtime/Common/IdsSource/IdsSource.cs
+++ Runtime/Common/IdsSource/IdsSource.cs
@@ -1,5 +1,10 @@
-﻿namespace Massive
+﻿using Unity.IL2CPP.CompilerServices;
+
+namespace Massive
 {
+	[Il2CppSetOption(Option.NullChecks, false)]
+	[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+	[Il2CppSetOption(Option.DivideByZeroChecks, false)]
 	public abstract class IdsSource
 	{
 		public int Count { get; set; }
--- Runtime/View/FilterViewExtensions.cs
+++ Runtime/View/FilterViewExtensions.cs
@@ -1,7 +1,11 @@
 ﻿using System.Runtime.CompilerServices;
+using Unity.IL2CPP.CompilerServices;
 
 namespace Massive
 {
+	[Il2CppSetOption(Option.NullChecks, false)]
+	[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+	[Il2CppSetOption(Option.DivideByZeroChecks, false)]
 	public static class FilterViewExtensions
 	{
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
