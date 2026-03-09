0735d30a57824819cfc00c80c275903ffd5359de
Daniil Pankevich <daniil.pankevich@gmail.com>
2026-03-09 12:46:30 +0300

    Fix allocator schema generation
    Both for primitive and for more complex stuff.

--- Runtime/Allocator/TypeSchemas/AllocatorSchemaGenerator.cs
+++ Runtime/Allocator/TypeSchemas/AllocatorSchemaGenerator.cs
@@ -52,7 +52,7 @@ namespace Massive
 
 			if (!IsUserStruct(type))
 			{
-				throw new InvalidOperationException("Type is not user struct.");
+				return AllocatorDataSchema.InvalidSchema;
 			}
 
 			var sizeOfType = ReflectionUtils.SizeOfUnmanaged(type);
@@ -65,7 +65,7 @@ namespace Massive
 			schema.ElementSize = (byte)sizeOfType;
 
 			var complexityCount = 0;
-			foreach (var (field, offset) in GetPointerFields(type, 0))
+			foreach (var (nestedType, field, offset) in GetPointerFields(type, 0))
 			{
 				var fieldType = field.FieldType;
 				var pointedType = fieldType.IsGenericType ? fieldType.GetGenericArguments()[0] : null;
@@ -113,7 +113,7 @@ namespace Massive
 					}
 
 					schema.OffsetSchemaCount[complexityCount - 1] |= AllocatorDataSchema.FlagMask;
-					schema.OffsetSchemaCount[complexityCount++] = (byte)Marshal.OffsetOf(type, pointerFieldAttribute.CountFieldName);
+					schema.OffsetSchemaCount[complexityCount++] = (byte)Marshal.OffsetOf(nestedType, pointerFieldAttribute.CountFieldName);
 				}
 			}
 
@@ -138,7 +138,7 @@ namespace Massive
 			return schemaIndex;
 		}
 
-		private static IEnumerable<(FieldInfo Field, int Offset)> GetPointerFields([Preserve(Member.PublicFields | Member.NonPublicFields)] Type type, int baseOffset)
+		private static IEnumerable<(Type Type, FieldInfo Field, int Offset)> GetPointerFields([Preserve(Member.PublicFields | Member.NonPublicFields)] Type type, int baseOffset)
 		{
 			if (!IsUserStruct(type))
 			{
@@ -156,7 +156,7 @@ namespace Massive
 
 				if (fieldType == typeof(Pointer) || genericTypeDefinition == typeof(Pointer<>))
 				{
-					yield return (field, offset + baseOffset);
+					yield return (type, field, offset + baseOffset);
 				}
 				else if (IsUserStruct(fieldType))
 				{
